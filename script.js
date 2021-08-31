8f427c2281baa607bf0dd65e61319f3e93294ffd
BadNerd <86760043+BadNerd@users.noreply.github.com>
2021-08-31 11:38:44 +0700

    add shuffle and reset button function

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Variables and Objects
+                // Variables and Objects\\
+ //slider 
 const sliderR = document.querySelector('input[name="sliderR"]');
 const sliderG = document.querySelector('input[name="sliderG"]')
 const sliderB = document.querySelector('input[name="sliderB"]');
 
+
+//text box
 const inputR = document.querySelector('input#inputR');
 const inputG = document.querySelector('input#inputG');
 const inputB = document.querySelector('input#inputB');
 
-
+//color box
 const colorBox = document.querySelector('div.container .color-box');
 
+//button
+const resetBtn = document.querySelectorAll('.button button')[0];
+const shuffleBtn = document.querySelectorAll('.button button')[1];
+
+// Default 
+sliderR.value = '0';
+sliderG.value = '0'; 
+sliderB .value = '0';
+
+inputR.value = '0';
+inputG.value = '0';
+inputB.value = '0';
+
+colorBox.style.backgroundColor = 'rgb(0,0,0)'
+
+                    // Functions \\
 // Slider
 sliderR.addEventListener('input',function()
 {
@@ -69,4 +88,33 @@ inputB.addEventListener('input',function(){
     {
         this.value = 0;
     }
+})
+// Button
+
+//reset button
+resetBtn.addEventListener('click',function(){
+    sliderR.value = '0';
+    sliderG.value = '0'; 
+    sliderB.value = '0';
+
+    const r = sliderR.value;
+    const g = sliderG.value;
+    const b = sliderB.value;
+
+    inputR.value = '0';
+    inputG.value = '0';
+    inputB.value = '0';
+
+    colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+})
+shuffleBtn.addEventListener('click',function(){
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+
+    sliderR.value = r;
+    sliderG.value = g;
+    sliderB.value = b;
+    
+    colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 })

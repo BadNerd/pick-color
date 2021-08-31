18bb338d9739d8448b1892e19e6a34c22f21210d
BadNerd <86760043+BadNerd@users.noreply.github.com>
2021-08-31 08:49:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-// // Variables and Objects
-// const sliderR = document.querySelector('input[name="sliderR"]');
-// const sliderG = document.querySelector('input[name="sliderG"]')
-// const sliderB = document.querySelector('input[name="sliderB"]');
+// Variables and Objects
+const sliderR = document.querySelector('input[name="sliderR"]');
+const sliderG = document.querySelector('input[name="sliderG"]')
+const sliderB = document.querySelector('input[name="sliderB"]');
 
-// const inputR = document.querySelector('input#inputR');
-// const inputG = document.querySelector('input#inputG');
-// const inputB = document.querySelector('input#inputB');
+const inputR = document.querySelector('input#inputR');
+const inputG = document.querySelector('input#inputG');
+const inputB = document.querySelector('input#inputB');
 
 
-// const colorBox = document.querySelector('div.container .color-box');
+const colorBox = document.querySelector('div.container .color-box');
 
-// // Slider
-// sliderR.addEventListener('input',function()
-// {
-//     const r = sliderR.value;
-//     const g = sliderG.value;
-//     const b = sliderB.value;
+// Slider
+sliderR.addEventListener('input',function()
+{
+    const r = sliderR.value;
+    const g = sliderG.value;
+    const b = sliderB.value;
 
-//     inputR.value = sliderR.value;
+    inputR.value = sliderR.value;
 
-//     colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-// })
-// sliderG.addEventListener('input',function()
-// {
-//     const r = sliderR.value;
-//     const g = sliderG.value;
-//     const b = sliderB.value;
+    colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+})
+sliderG.addEventListener('input',function()
+{
+    const r = sliderR.value;
+    const g = sliderG.value;
+    const b = sliderB.value;
 
-//     inputG.value = sliderG.value;
+    inputG.value = sliderG.value;
 
-//     colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-// })
-// sliderB.addEventListener('input',function()
-// {
-//     const r = sliderR.value;
-//     const g = sliderG.value;
-//     const b = sliderB.value;
+    colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+})
+sliderB.addEventListener('input',function()
+{
+    const r = sliderR.value;
+    const g = sliderG.value;
+    const b = sliderB.value;
 
-//     inputB.value = sliderB.value;
+    inputB.value = sliderB.value;
 
-//     colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-// })
+    colorBox.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+})
 
-// // Text Box
-// inputR.addEventListener('input',function(){
-//     sliderR.value = this.value;
-//     if(this.value > 255){
-//         this.value = 255;
-//     }else if(this.value < 0)
-//     {
-//         this.value = 0;
-//     }
-// })
-// inputG.addEventListener('input',function(){
-//     sliderG.value = this.value;
-//     if(this.value > 255){
-//         this.value = 255;
-//     }else if(this.value < 0)
-//     {
-//         this.value = 0;
-//     }
-// })
-// inputB.addEventListener('input',function(){
-//     sliderB.value = this.value;
-//     if(this.value > 255){
-//         this.value = 255;
-//     }else if(this.value < 0)
-//     {
-//         this.value = 0;
-//     }
-// })
+// Text Box
+inputR.addEventListener('input',function(){
+    sliderR.value = this.value;
+    if(this.value > 255){
+        this.value = 255;
+    }else if(this.value < 0)
+    {
+        this.value = 0;
+    }
+})
+inputG.addEventListener('input',function(){
+    sliderG.value = this.value;
+    if(this.value > 255){
+        this.value = 255;
+    }else if(this.value < 0)
+    {
+        this.value = 0;
+    }
+})
+inputB.addEventListener('input',function(){
+    sliderB.value = this.value;
+    if(this.value > 255){
+        this.value = 255;
+    }else if(this.value < 0)
+    {
+        this.value = 0;
+    }
+})
